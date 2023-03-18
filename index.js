738d2192ff63068e69d061bf8f21d838c941fe5c
james <astonjames@iahni.com>
2023-03-17 18:06:00 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const welcomeMessage = ' Welcome to Flatbook, ${james} !' ;
+const currentUser = 'james';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
